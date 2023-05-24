8d60de30e44b3e6145e19abb122f1348c83bdda9
Imon <contact@imonweb.co.uk>
2023-05-24 09:30:04 +0100

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import UseStateBasics from './tutorial/01-useState/starter/02-useState-basics'
 import './App.css'
 
 function App() {
   return (
       <div className='container'>
-        <h1>useState Basics</h1>
+        {/* <h1>useState Basics</h1> */}
+        <UseStateBasics />
       </div>
   )
 }
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 
 const UseStateBasics = () => {
-  console.log(useState());
-  return <h2>useState basics</h2>;
+  
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState()
+
+  const handleClick = () => {
+    setCount(count + 1)
+    
+  }
+  return <div>
+    <h4>You clicked {count} times </h4>
+    <button type='button' className="btn" onClick={handleClick}>click me</button>
+  </div>
 };
 
 export default UseStateBasics;
