5a19dec03671466f8d5117ef81535cf69651aae1
Imon <contact@imonweb.co.uk>
2023-05-25 17:33:41 +0100

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-import UseStateBasics from './tutorial/01-useState/starter/02-useState-basics'
+// import UseStateBasics from './tutorial/01-useState/starter/02-useState-basics'
 import './App.css'
+import UseStateArray from './tutorial/01-useState/starter/03-useState-array'
+import { data } from './data'
+
+import Starter from './tutorial/01-useState/starter/03-useState-array';
+// import Final from './tutorial/01-useState/final/03-useState-array';
+
 
 function App() {
   return (
       <div className='container'>
         {/* <h1>useState Basics</h1> */}
-        <UseStateBasics />
+        {/* <UseStateBasics /> */}
+        {/* <UseStateArray /> */}
+        <Starter />
+        {/* <Final /> */}
+
       </div>
   )
 }
